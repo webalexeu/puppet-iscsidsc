e8826601ae42f960ae83597aaa4291f701b7958e
Alexandre JARDON <28548335+webalexeu@users.noreply.github.com>
2025-02-27 09:23:21 +0100

    Update dsc_iscsivirtualdisk.rb

--- lib/puppet/provider/dsc_iscsivirtualdisk/dsc_iscsivirtualdisk.rb
+++ lib/puppet/provider/dsc_iscsivirtualdisk/dsc_iscsivirtualdisk.rb
@@ -1,4 +1,4 @@
-require 'puppet/provider/dsc_base_provider/dsc_base_provider'
+require_relative '../../../../../pwshlib/lib/puppet/provider/dsc_base_provider/dsc_base_provider'
 
 # Implementation for the dsc_type type using the Resource API.
 class Puppet::Provider::DscIscsivirtualdisk::DscIscsivirtualdisk < Puppet::Provider::DscBaseProvider
